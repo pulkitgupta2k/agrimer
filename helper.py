ea996aa687420eee772c17d3a6c6608c7ae354f1
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-24 17:11:34 +0530

    made different cat

--- helper.py
+++ helper.py
@@ -5,14 +5,13 @@ from pprint import pprint
 from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+import re
 
 def getSoup(link):
     req = requests.get(link)
     html = req.content
     soup = BeautifulSoup(html, "html.parser")
     return soup
-
 
 def append_rows(self, values, value_input_option='RAW'):
     params = {
@@ -25,35 +24,64 @@ def append_rows(self, values, value_input_option='RAW'):
     return self.spreadsheet.values_append(self.title, params, body)
 
 
-
-def get_all_products():
+def get_all_products(link, file, start=0, end=9999):
     product_links = {}
     product_links["links"] = []
-    link = "https://rnm.franceagrimer.fr/prix?FRUITS-ET-LEGUMES"
+    # link = "https://rnm.franceagrimer.fr/prix?FRUITS-ET-LEGUMES"
     soup = getSoup(link)
     products = soup.findAll("div", {"class": "listunproduit"})
+    ctr = 0
     for product in products:
         link = "https://rnm.franceagrimer.fr" + product.find("a")['href']
-        product_links["links"].append(link)
-    with open("product_links.json", "w") as f:
+        if ctr >= start and ctr <= end:
+            product_links["links"].append(link)
+        ctr = ctr + 1
+    with open(file, "w") as f:
         json.dump(product_links,f)
+
+def get_all_products_driver():
+    get_all_products("https://rnm.franceagrimer.fr/prix?FRUITS", "fruits_a_n.json", 0, 41)
+    get_all_products("https://rnm.franceagrimer.fr/prix?FRUITS", "fruits_o_z.json",42)
+    get_all_products("https://rnm.franceagrimer.fr/prix?LEGUMES", "legumes_a_c.json",0,32)
+    get_all_products("https://rnm.franceagrimer.fr/prix?LEGUMES", "legumes_d_z.json",33)
+    get_all_products("https://rnm.franceagrimer.fr/prix?PECHE-ET-AQUACULTURE", "peche_aquaculture.json")
+    get_all_products("https://rnm.franceagrimer.fr/prix?VIANDE", "viande.json")
+    get_all_products("https://rnm.franceagrimer.fr/prix?BEURRE-OEUF-FROMAGE", "beurre.json")
 
 def get_product_type(link):
     soup = getSoup(link)
     soup = soup.find("table", {"class": "tabcot"})
+    soup = soup.find("tbody")
     rows = soup.findAll("tr")
     dets = []
+    stade = ''
+    marche = ''
+    unite = ''
     for row in rows:
         onclick = row.find("td", {"class": "tdcotl"})
         if not onclick == None:
             det = {}
-            det = onclick.find("a")
-            link = det['onclick'][10:-4]
-            name = det.text
+            d = onclick.find("a")
+            link = d['onclick'][10:-4]
+            name = d.text
             det['name'] = name.strip()
+            det['stade'] = stade
+            det['marche'] = marche
+            det['unite'] = unite
             det['link'] = link
             dets.append(det)
+        else:
+            try:
+                stade = row.text
+                stade = re.search(r'\(cours (.*?)\)', stade).group(1)
+                marche = row.find("strong").text
+                unite = row.text
+                unite = re.search(r'unité :(.*?)\*', unite).group(1)
+            except:
+                pass
     return dets
+
+
 
 def get_type_dets(libcod):
     information = []
@@ -74,7 +102,21 @@ def make_dates_json():
     dates = {}
     dates['dates'] = []
     temp = []
-    with open('all_details.json', 'r') as f:
+    temp = temp.extend(dates_separate("fruits_a_n_details.json"))
+    temp = temp.extend(dates_separate("fruits_o_z_details.json"))
+    temp = temp.extend(dates_separate("legumes_a_c_details.json"))
+    temp = temp.extend(dates_separate("legumes_d_z_details.json"))
+    temp = temp.extend(dates_separate("peche_aquacuture_details.json"))
+    temp = temp.extend(dates_separate("viande_details.json"))
+    temp = temp.extend(dates_separate("beurre_details.json"))
+    temp.sort(key=lambda date: datetime.strptime(date, "%d/%m/%y"), reverse=True)
+    dates['dates'] = temp
+    with open('dates.json', 'w') as f:
+        json.dump(dates, f)
+
+def dates_separate(file):
+    temp = []
+    with open(file, 'r') as f:
         details = json.load(f)
     for product in details['data']:
         for types in product['types']:
@@ -82,47 +124,44 @@ def make_dates_json():
             for date in dates_:
                 if date[0] not in temp:
                     temp.append(date[0])
-    temp.sort(key=lambda date: datetime.strptime(date, "%d/%m/%y"), reverse=True)
-    dates['dates'] = temp
-    with open('dates.json', 'w') as f:
-        json.dump(dates, f)
+    return temp
 
-def format_data():
-    with open('all_details copy.json', 'r') as f:
-        details = json.load(f)
+# def format_data():
+#     with open('all_details copy.json', 'r') as f:
+#         details = json.load(f)
     
-    ret_dets = {}
-    ret_dets['data'] = []
+#     ret_dets = {}
+#     ret_dets['data'] = []
 
-    for product in details['data']:
-        ret_product = {}
-        ret_product['name'] = product['name']
-        ret_product['types'] = []
-        for types in product['types']:
-            t = []
-            t.append(types[0].strip())
-            dates_ = types[1]
-            d = []
-            for date in dates_:
-                dt = []
-                dt.append(date[0].strip())
-                dt.append(date[1].strip())
-                dt.append(date[2].strip())
-                dt.append(date[3].strip())
-                d.append(dt)
-            t.append(d)
-            ret_product['types'].append(t)
-        # print(ret_product)
-        ret_dets['data'].append(ret_product)
+#     for product in details['data']:
+#         ret_product = {}
+#         ret_product['name'] = product['name']
+#         ret_product['types'] = []
+#         for types in product['types']:
+#             t = []
+#             t.append(types[0].strip())
+#             dates_ = types[1]
+#             d = []
+#             for date in dates_:
+#                 dt = []
+#                 dt.append(date[0].strip())
+#                 dt.append(date[1].strip())
+#                 dt.append(date[2].strip())
+#                 dt.append(date[3].strip())
+#                 d.append(dt)
+#             t.append(d)
+#             ret_product['types'].append(t)
+#         # print(ret_product)
+#         ret_dets['data'].append(ret_product)
     
     
-    with open('all_details.json', 'w') as f:
-        json.dump(ret_dets, f)
+#     with open('all_details.json', 'w') as f:
+#         json.dump(ret_dets, f)
 
-def get_all_details():
+def get_single_details(inp_file, out_file):
     details = {}
     details['data'] = []
-    with open("product_links.json") as f:
+    with open(inp_file) as f:
         product_links = json.load(f)['links']
     for product_link in product_links:
         product = {}
@@ -133,13 +172,25 @@ def get_all_details():
         for t in types:
             type_detail = []
             type_detail.append(t['name'])
+            type_detail.append(t['stade'])
+            type_detail.append(t['marche'])
+            type_detail.append(t['unite'])
             type_detail.append(get_type_dets(t['link']))
             product["types"].append(type_detail)
             print(".")
         pprint(product)
         details['data'].append(product)
-    with open('all_details.json', 'w') as f:
+    with open(out_file, 'w') as f:
         json.dump(details,f)
+
+def get_all_details():
+    get_single_details("fruits_a_n.json", "fruits_a_n_details.json")
+    get_single_details("fruits_o_z.json", "fruits_o_z_details.json")
+    get_single_details("legumes_a_c.json", "legumes_a_c_details.json")
+    get_single_details("legumes_d_z.json", "legumes_d_z_details.json")
+    get_single_details("peche_aquacuture.json", "peche_aquacuture_details.json")
+    get_single_details("viande.json", "viande_details.json")
+    get_single_details("beurre.json", "beurre_details.json")
 
 
 def make_matrix():
@@ -150,8 +201,6 @@ def make_matrix():
 
     ret_dets = []
 
-
-
     for product in details['data']:
         ret_product = {}
         ret_product['name'] = product['name']
@@ -160,19 +209,27 @@ def make_matrix():
 
         for d in dates['dates']:
             heading.append(d)
-        heading.insert(0, 'Product')
+        heading.insert(0, 'unite')
+        heading.insert(0, 'libelle')
+        heading.insert(0, 'marche')
+        heading.insert(0, 'stade')
+        heading.insert(0, 'product')
         ret_product['rows'].append(heading)
 
         for types in product['types']:
             t = []
             for d in dates['dates']:
                 t.append('')
-            dates_price = types[1]
+            dates_price = types[5]
             for date in dates_price:
                 for index, d in enumerate(dates['dates']):
                     if date[0] == d:
                         t[index] = date[1]
                         break
+            t.insert(0, types[4])
+            t.insert(0, types[3])
+            t.insert(0, types[2])
+            t.insert(0, types[1])
             t.insert(0, types[0])
             ret_product['rows'].append(t)
         ret_dets.append(ret_product)
@@ -210,3 +267,7 @@ def gsheet_load():
                 worksheet = agrimer.add_worksheet(title= matrix['name'], rows="100", cols="20")
             worksheet.clear()
             append_rows(worksheet, matrix['rows'])
+
+# get_all_products_driver()
+# get_product_type("https://rnm.franceagrimer.fr/prix?ABRICOT")
+get_all_details()
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from helper import *
 if __name__ == "__main__":
     get_all_details()
     make_dates_json()
-    gsheet_load()
+    # gsheet_load()
