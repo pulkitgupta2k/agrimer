c2550b57ae6af32c02e5c2e60fd6ca15f5673326
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-25 17:36:03 +0530

    server ready

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import time
 if __name__ == "__main__":
     while True:
         get_all_details()
-        # make_dates_json()
         gsheet_load()
         time.sleep(86400)
+        print("sleeping...")
